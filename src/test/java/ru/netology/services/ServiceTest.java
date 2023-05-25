7a0cf1ad80a07db49dc78155cf8373daee922973
mayer72 <mayer7272@mail.ru>
2023-05-25 17:18:51 +0500

    testProgram2

--- src/test/java/ru/netology/services/ServiceTest.java
+++ src/test/java/ru/netology/services/ServiceTest.java
@@ -7,7 +7,15 @@ import org.junit.jupiter.api.Test;
 
 public class ServiceTest {
     @Test
-    public void testProgram(){
+    public void testProgram1(){
+        Service service = new Service();
+        int expected = 3;
+        int actual = service.calculate(10000, 3000, 20000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testProgram2(){
         Service service = new Service();
         int expected = 2;
         int actual = service.calculate(100000, 60000, 150000);
